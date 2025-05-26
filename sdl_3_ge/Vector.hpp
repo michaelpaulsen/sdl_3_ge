2ad7b7d58f9ca6f1ad1fc4d861c01ac7e1e4b754
michaelpaulsen <michael.j.paulsen@gmail.com>
2025-05-26 09:38:02 -0700

    [A 6.2.0] added formatter for vect2 class.

--- sdl_3_ge/Vector.hpp
+++ sdl_3_ge/Vector.hpp
@@ -1,7 +1,8 @@
 #pragma once
 #include <concepts>
 #include <cmath> 
-
+#include <format>
+#include <typeinfo>
 namespace SKC::Math {
 template <typename T> concept numeber = std::is_integral_v<T> || std::is_floating_point_v<T>; 
 	template<numeber T>
@@ -48,3 +49,34 @@ template <typename T> concept numeber = std::is_integral_v<T> || std::is_floatin
 using skc_Vect2d = SKC::Math::Vect2<double>;
 using skc_Vect2f = SKC::Math::Vect2<float>;
 using skc_Vect2i = SKC::Math::Vect2<int>;
+
+
+//TODO(SKC): move to own file.
+// this should be in own file because there is no other reason that format is included in this file.
+//TODO(SKC) : make this so that it only outputs in dbg mode... 
+template <typename T>
+struct std::formatter<SKC::Math::Vect2<T>> {
+	bool show_type = false;
+	constexpr auto parse(std::format_parse_context& ctx) {
+		auto pos = ctx.begin();
+		while (pos != ctx.end() && *pos != '}') {
+			show_type = (*pos == '#');
+			++pos;
+		}
+		return pos;
+	}
+
+	auto format(const SKC::Math::Vect2<T>& obj, std::format_context& ctx) const {
+
+		std::string fmt_string = "";
+		if (show_type) {
+			fmt_string += "vec2<";
+			fmt_string += typeid(T).name();
+			fmt_string += ">";
+
+		}
+		fmt_string += std::format("({}, {})", obj.x, obj.y);
+		return std::format_to(ctx.out(), "{}", fmt_string);
+	}
+
+};
