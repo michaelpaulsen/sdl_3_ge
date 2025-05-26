6410b626516a19546c909b434f21c41efd39488f
michaelpaulsen <michael.j.paulsen@gmail.com>
2025-05-26 09:37:17 -0700

    [A 6.1.0] added misc features to vect2 class.

--- sdl_3_ge/Vector.hpp
+++ sdl_3_ge/Vector.hpp
@@ -4,16 +4,24 @@
 
 namespace SKC::Math {
 template <typename T> concept numeber = std::is_integral_v<T> || std::is_floating_point_v<T>; 
-	template<std::floating_point T> 
+	template<numeber T>
 	struct Vect2 {
 		T x{}, y{};
 		
 		Vect2(T _x, T _y) : x(_x), y(_y) {}; 
+		static Vect2<T>  from_polar(double angle, double length = 1) {
+			return{ static_cast<T>(std::sin(angle) * length), static_cast<T>(-std::cos(angle) * length) };
+		}
 		double len() {
 			return std::sqrt((x * x) + (y * y)); 
 		}
 		T len2() {
 			return (x * x) + (y * y);
+		}
+		void normalize() {
+			double l = len();
+			x /= l;
+			y /= l; 
 		}
 		T dot(Vect2<T> other) {
 			return (x * other.x) + (y * other.y); 
