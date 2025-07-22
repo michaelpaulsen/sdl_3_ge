88b730a650646bba009ed6b0eebe89f4af33c482
michaelpaulsen <michael.j.paulsen@gmail.com>
2025-07-21 20:13:41 -0700

    [A8.0.0] Enhance texture management in Window.hpp
    added SDL_image_texture_wrapper, wich is a RAII wrapper around the SDL_Texture resource that also stores a referance ID, as a private struct defention to Window.
    Made Window own its textures by adding  a vector of SDL_image_texture_wrappers, and introduced new methods for drawing textures by ID (which is not its index) to prevent having the consumer of the library from having to pass raw pointers around.
    deprecated create_surface in favor of using textures.
    Renamed set_window_title to set_title and added comments for clarity on function usage and safety.

--- sdl_3_ge/Window.hpp
+++ sdl_3_ge/Window.hpp
@@ -3,11 +3,11 @@
 #include <filesystem>
 #include <SDL3/SDL.h>
 #include <SDL3_image/SDL_image.h>
-
+#include <map>
 //this is where the color stuff is stored. 
 #include "Color.hpp"
 namespace SKC::GE {
-	
+
 	
 	using point  = SDL_Point;
 	using rect   = SDL_Rect; 
@@ -17,14 +17,53 @@ namespace SKC::GE {
 	//stuff to own file. 
 	namespace fs = std::filesystem; 
 	using path_t = fs::path; 
-	
 	class window {
+	protected:
+		
+		struct SDL_image_texture_wrapper {
+			inline static size_t next_tid = 0;
+			SDL_Texture* tex;
+			fs::path path;
+			size_t tid;
+			SDL_image_texture_wrapper(SDL_Texture* _t, fs::path pth = fs::path()) : tid(++next_tid), tex(_t), path(pth) {
+
+			}
+			~SDL_image_texture_wrapper() {
+				SDL_DestroyTexture(tex);
+			}
+			SDL_image_texture_wrapper(SDL_image_texture_wrapper&& other) noexcept {
+				tid = ++next_tid;
+				tex = other.tex;
+				other.tex = nullptr;
+				other.tid = 0;
+
+			}
+			//NOTE(skc) we may have to implement these; 
+			SDL_image_texture_wrapper(const SDL_image_texture_wrapper& other) = delete;
+			SDL_image_texture_wrapper operator=(const SDL_image_texture_wrapper&) = delete;
+			SDL_image_texture_wrapper operator=(SDL_image_texture_wrapper&& other) noexcept
+			{
+				if (this == &other) return std::move(*this); //self assignment check
+				tid = ++next_tid;
+				this->~SDL_image_texture_wrapper(); //destroy the current object
+				this->path = other.path; //copy the path
+				this->tex = other.tex; //move the texture pointer
+				other.tex = nullptr; //nullify the other texture pointer
+				tex = other.tex;
+				other.tid = 0;
+				return std::move(*this); 
+			};
+
+			bool operator ==(const SDL_image_texture_wrapper& other) const { return path == other.path; }
+			bool operator ==(const fs::path& other) const { return path == other; }
+			bool operator== (size_t other) const { return tid == other; }
+
+		};
 	protected: 
 		using c_t = Uint8;
-
-		 
+		std::vector< SDL_image_texture_wrapper> m_image_textures{};
 		int m_x{}, m_y{}, m_width, m_height;
-		std::string title; 
+		std::string title;
 		float m_scale_x{ 1 }, m_scale_y{ 1 };
 		SDL_Window* m_window;
 		SDL_Renderer* m_renderer; 
@@ -58,7 +97,7 @@ namespace SKC::GE {
 		auto get_renderer() {
 			return m_renderer; 
 		}
-
+		
 		[[depercated ("CREATE AN IMAGE TEXTURE INSTEAD")]] auto create_surface() {
 			int s_width = (int)m_width / (int)m_scale_x; 
 			int s_height = (int)m_height / (int)(int)m_scale_y;
@@ -102,7 +141,7 @@ namespace SKC::GE {
 				//return tex; 
 
 		}
-
+		
 		void update_window_size() {
 			rect ret{};
 			SDL_GetRenderSafeArea(m_renderer, &ret);
@@ -153,7 +192,7 @@ namespace SKC::GE {
 		const std::string get_window_title() const {
 			return SDL_GetWindowTitle(m_window);
 		}
-		void set_window_title(std::string title) {
+		void set_title(std::string title) {
 			SDL_SetWindowTitle(m_window, title.c_str());
 		}
 
@@ -226,13 +265,26 @@ namespace SKC::GE {
 		void present() {
 			SDL_RenderPresent(m_renderer);
 		}
-
+		
 		
 		/*
 		*--texture API-- 
 		*this is where all of the code 
 		*that involves rendering textures goes 
 		*/
+		
+		//NOTE(skc): YOU REALLY SHOULD NOT USE THIS MEMBER FUNCTION (AS IT RETURNS A RAW POINTER!!!).
+		//it is only public for the SDL functions that are not wrapped in the ID based API yet. 
+		//once I am sure that all of the SDL functions are wrapped in the ID based API I will make this private.
+		//I make no guarantees that this will not cause a use after free error.
+		//Though it should not be possible based off of how classes work in C++.
+		auto get_tex_from_tid(size_t tid) {
+			for (const auto& texture : m_image_textures) {
+				if (texture == tid) {
+					return texture.tex;
+				}
+			}
+		}
 		void draw_texture(SDL_Texture *txt) {
 			SDL_RenderTexture(m_renderer, txt, NULL, NULL);
 		}
@@ -254,6 +306,41 @@ namespace SKC::GE {
 			SDL_RenderTextureRotated(m_renderer, txt, NULL, &pos, angle, NULL, flip); 
 		}
 		void draw_texture_rotated(SDL_Texture* txt, const Frect atlas_pos, const Frect pos, const Fpoint center, const double angle, SDL_FlipMode flip = SDL_FLIP_NONE) {
+			SDL_RenderTextureRotated(m_renderer, txt, &atlas_pos, &pos, angle, &center, flip);
+
+		}
+
+//The ID based Texture format API 
+		//NOTE(skc): this is the API that you should be using by default. the only reason 
+		//you would use the SDL_Texture* API is if you are using a texture that is generated progrimatically.
+		
+		void draw_texture(size_t tid) {
+			auto txt = get_tex_from_tid(tid); 
+			if (!txt) return; 
+			SDL_RenderTexture(m_renderer, txt, NULL, NULL);
+		}
+		void draw_texture(size_t tid, Frect pos) {
+			auto txt = get_tex_from_tid(tid);
+			SDL_RenderTexture(m_renderer, txt, NULL, &pos);
+		}
+		void draw_texture(size_t tid, Frect pos, Frect atlas_pos) {
+			auto txt = get_tex_from_tid(tid);
+			SDL_RenderTexture(m_renderer, txt, &atlas_pos, &pos);
+		}
+		void draw_texture_with_afine_transform(size_t tid, Fpoint tl, Fpoint tr, Fpoint bl) {
+			auto txt = get_tex_from_tid(tid);
+			SDL_RenderTextureAffine(m_renderer, txt, NULL, &tl, &tr, &bl);
+		}
+		void draw_texture_rotated(size_t tid, Frect atlas_pos, Frect pos, double angle, SDL_FlipMode flip = SDL_FLIP_NONE) {
+			auto txt = get_tex_from_tid(tid);
+			SDL_RenderTextureRotated(m_renderer, txt, &atlas_pos, &pos, angle, NULL, flip);
+		}
+		void draw_texture_rotated(size_t tid, const Frect pos, const double angle, const SDL_FlipMode flip = SDL_FLIP_NONE) {
+			auto txt = get_tex_from_tid(tid);
+			SDL_RenderTextureRotated(m_renderer, txt, NULL, &pos, angle, NULL, flip);
+		}
+		void draw_texture_rotated(size_t tid, const Frect atlas_pos, const Frect pos, const Fpoint center, const double angle, SDL_FlipMode flip = SDL_FLIP_NONE) {
+			auto txt = get_tex_from_tid(tid);
 			SDL_RenderTextureRotated(m_renderer, txt, &atlas_pos, &pos, angle, &center, flip);
 
 		}
