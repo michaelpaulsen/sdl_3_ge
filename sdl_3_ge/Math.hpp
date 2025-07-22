28304524614358a085551301b0dadf32ecc75ec1
michaelpaulsen <michael.j.paulsen@gmail.com>
2025-07-21 18:14:52 -0700

    [A7.2.0] Enhance Math functions and improve type safety
    - Added `#define U32` for static casting to `uint32_t`.
    - Introduced `wrap` function for value range wrapping.
    - Implemented `decay` function for value reduction with threshold.
    - Updated `noise2d` to use `U32` for coordinate casting.
    - Ensured `pow` result in `noise2d` is cast to `U32`.
    - Refactored `smooth_noise2d`.

--- sdl_3_ge/Math.hpp
+++ sdl_3_ge/Math.hpp
@@ -1,9 +1,34 @@
 #pragma once
 #include <numbers>
 #include "./perlin.hpp"
+#define U32 static_cast<uint32_t>
 namespace SKC::Math {
 	static constexpr inline double DEG_TO_RAD = (std::numbers::pi / 180.);
 	static constexpr inline double RAD_TO_DEG = (180./ std::numbers::pi);
+	double  wrap(double val, double min, double max) {
+		double ret = val; 
+		while (ret < min) { 
+			ret += max; 
+		}
+		while (ret > max) {
+			ret -= min; 
+		}
+		return ret; 
+	}
+	double  decay(double val, double decay = .5,  double t = 1e-5 ) {
+		if (decay < 0) decay = -decay; 
+		bool is_neg = false; 
+		if (val < 0) {
+			is_neg = true; 
+			val *= -1;
+		}
+		val *= decay; 
+		if (val <= t) return 0;
+		if (is_neg) val *= -1; 
+	
+		return val; 
+		
+	}
 	double map(double t, double min, double max, double to_min, double to_max) {
 		return (t - min) / (max - min) * (to_max - to_min) + to_min;
 	}
@@ -21,17 +46,18 @@ namespace SKC::Math {
 	}
 	double noise2d(double x, double y) {
 		
-		uint32_t n = x + y * 57; 
+		uint32_t n  = U32(x) + U32(y) * 57;
 		//uint32_t tn = n; 
-		n = pow(n<<13,n);
+		//pow for some reason returns double... 
+		n = U32(pow(n<<13,n));
 		return (1.0 - ((n * (n * n * 15731 + 789221) + 1376312589) & 0x7fffffff) / 1073741824.0);
 
 	}
 	double smooth_noise2d(double x, double y) {
-		for (int xoff = -1; xoff <= 1; ++xoff) {
 			double crn = 0; 
 			double edge = 0;
 			double center = 0; 
+		for (int xoff = -1; xoff <= 1; ++xoff) {
 
 			if (x == 0 && xoff < 0) continue; 
 			for (int yoff = -1; yoff <= 1; ++yoff) {
@@ -47,9 +73,8 @@ namespace SKC::Math {
 				}
 				crn += val; 
 			}
+		}
 			auto ret = crn / 16 + edge / 8 + center / 4; 
 			return ret;
 		}
-		
-	}
 }
