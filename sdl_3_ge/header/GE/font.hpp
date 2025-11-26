db4e599243ebb5f5f9d9d8b4c918978814ed7617
michaelpaulsen <michael.j.paulsen@gmail.com>
2025-11-25 16:23:42 -0800

    [A20.0.0] added font class.
    as of right now the font class just acts like an RAII class over the TTF_font type but more is planned

---
+++ sdl_3_ge/header/GE/font.hpp
@@ -0,0 +1,63 @@
+#pragma once
+#include <SDL3_ttf/SDL_ttf.h>
+#include <filesystem>
+namespace SKC::GE {
+	enum class FONT_CHANGE_ERROR {
+		E_OKAY,
+		E_NO_CHANGE,
+		E_SDL_ERROR
+	};
+	class font {
+		std::filesystem::path m_path; 
+		unsigned int m_size{}, m_outline{0};
+		TTF_Font *m_font; 
+	public:
+		font() = delete;
+		font(std::filesystem::path path, int size): m_path(path),  m_size(size) {
+			m_font = TTF_OpenFont(path.string().c_str(), size); 
+		};
+		font(font&) = delete; 
+		font(font&& other) noexcept {
+			m_path = other.m_path;
+			m_font = other.m_font; 
+			m_size = other.m_size; 
+			m_outline = other.m_outline; 
+			other.m_font = nullptr; 
+
+		}
+		~font()
+		{
+			TTF_CloseFont(m_font);
+		}
+		font operator=(font&) = delete; 
+		font operator=(font&& other) noexcept {
+			*this = std::move(other); 
+		}
+		
+		font operator=(std::filesystem::path path) {
+			
+			TTF_CloseFont(m_font);
+			m_font = TTF_OpenFont(path.string().c_str(), m_size);
+			if (m_outline) {
+				TTF_SetFontOutline(m_font, m_outline);
+			}
+
+		}
+		
+		void make_out_line_font(int outline_size) {
+			TTF_SetFontOutline(m_font, outline_size); 
+			m_outline = outline_size;
+		}
+		void set_size(int size) {
+			if (size == m_size) return;
+			m_size = size;
+			TTF_SetFontSize(m_font, size); 
+		}
+		TTF_Font* operator*() {
+			return m_font; 
+		}
+		bool operator!() {
+			return !m_font;
+		}
+	};
+}
