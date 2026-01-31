bb5ab6a7ebbaf4719e5074df5eea8c85cb98b8bb
Michael Paulsen <michael.j.paulsen@gmail.com>
2026-01-30 18:00:44 -0800

    [A23.9.0] Add choose_from for random element selection from containers
    Introduce choose_from template to select a random element from a random access container, returning std::optional<T> and handling errors with diagnostic messages. Add necessary headers and improve RNG template formatting.

--- sdl_3_ge/header/math/rand.hpp
+++ sdl_3_ge/header/math/rand.hpp
@@ -1,14 +1,47 @@
 #pragma once
 #include <random>
-#include <cstdint>
-#include <type_traits>
 #include <algorithm>
+#include <optional>
+#include <memory>
 
 #include "../concepts.hpp"
 namespace SKC::GE {
+	
+	template<
+		typename T,
+		template <typename t, typename aloc = std::allocator<t>> 
+			typename container_,
+		std::uniform_random_bit_generator random_src = std::mt19937
+	>
+		requires concepts::random_access_container<container_,T> 
+		
+	std::optional<T> choose_from(container_<T> container) noexcept {
+		random_src generator(std::random_device{}());
+		std::uniform_int_distribution<size_t> distribution(0, container.size() - 1);
+		auto index = distribution(generator);
+		if (container.size() == 0) {
+			std::println("[RNG choose_from] empty container");
+			return {}; 
+		}
+		T result{};
+		try {
+			result = container.at(index);
+		}
+		catch (const std::out_of_range& e) {
+			std::println("[RNG choose_from] out of range access at index {}", index);
+			std::println("[RNG choose_from] returning last value", index);
+			return {}; 
+		}
+		catch (...) {
+			std::println("[RNG choose_from] unknown error accessing index {}", index);
+			return {}; 
+		}
+		return  result; 
 
+	}
 
-	template<concepts::number T, std::uniform_random_bit_generator random_src = std::mt19937>
+	template<concepts::number T,
+		std::uniform_random_bit_generator random_src = std::mt19937>
 	class RNG {
 		random_src m_generator;
 	public:
