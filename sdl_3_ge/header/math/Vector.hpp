4561a5337e881929a42ca4f7dd1b3ababca62318
Michael Paulsen <michael.j.paulsen@gmail.com>
2026-01-31 00:13:05 -0800

    [A23.10.0] added a scalar operator / to vec2 class, made the non-saclar operators templated.
    Made the non-saclar  operators templated so that you don't have to cast to do math on the vectors.

--- sdl_3_ge/header/math/Vector.hpp
+++ sdl_3_ge/header/math/Vector.hpp
@@ -1,20 +1,19 @@
 #pragma once
-#include <concepts>
 #include <cmath> 
 #include <format>
 #include <typeinfo>
 namespace SKC::Math {
-template <typename T> concept number = std::is_integral_v<T> || std::is_floating_point_v<T>; 
+	template <typename T> concept number = std::is_integral_v<T> || std::is_floating_point_v<T>;
 	template<number T>
 	struct Vect2 {
 		T x{}, y{};
-		
-		Vect2(T _x, T _y) : x(_x), y(_y) {}; 
+
+		Vect2(T _x, T _y) : x(_x), y(_y) {};
 		static Vect2<T>  from_polar(double angle, double length = 1) {
 			return{ static_cast<T>(std::sin(angle) * length), static_cast<T>(-std::cos(angle) * length) };
 		}
 		double len() {
-			return std::sqrt((x * x) + (y * y)); 
+			return std::sqrt((x * x) + (y * y));
 		}
 		T len2() {
 			return (x * x) + (y * y);
@@ -22,30 +21,46 @@ template <typename T> concept number = std::is_integral_v<T> || std::is_floating
 		void normalize() {
 			double l = len();
 			x /= l;
-			y /= l; 
+			y /= l;
 		}
 		T dot(Vect2<T> other) {
-			return (x * other.x) + (y * other.y); 
+			return (x * other.x) + (y * other.y);
 		}
-		void operator+=(T other) {
+
+		//scalar operations
+		void operator+=(number auto other) {
 			x += other;
 			y += other;
 		}
-		void operator+=(Vect2<T> other) {
-			x += other.x; 
-			y += other.y; 
-		}
-		Vect2<T> operator + (Vect2<T> other) {
-			return { x + other.x, y + other.y };
-		}
-		Vect2<T> operator - (Vect2<T> other) {
-			return { x - other.x, y - other.y }; 
-		}
 		Vect2<T> operator * (number auto scalar) {
-			return { x * scalar, y * scalar }; 
+			return { x * scalar, y * scalar };
 		}
 		Vect2<T> operator / (number auto scalar) {
 			return { x / scalar, y / scalar };
+		}
+	
+		template<typename Y> 
+		void operator+=(Vect2<Y> other) {
+			x += other.x;
+			y += other.y;
+		}
+
+		template<typename Y>
+		Vect2<T> operator + (Vect2<Y> other) {
+			return { x + other.x, y + other.y };
+		}
+		template<typename Y>
+		Vect2<T> operator - (Vect2<Y> other) {
+			return { x - other.x, y - other.y };
+		}
+		
+		//NOTE(skc) : this is a template so that it 
+		//behaves like normal math... 
+
+		//TODO(skc) : name this template paramiter something better
+		template<number Y>
+		Vect2<T> operator/ (Vect2<Y> divisor) {
+			return { x / divisor.x, y / divisor.y };
 		}
 
 		template<number U>
@@ -57,10 +72,9 @@ template <typename T> concept number = std::is_integral_v<T> || std::is_floating
 	using Vect2f = SKC::Math::Vect2<float>;
 	using Vect2i = SKC::Math::Vect2<int>;
 }
-using skc_Vect2d = SKC::Math::Vect2<double>;
-using skc_Vect2f = SKC::Math::Vect2<float>;
-using skc_Vect2i = SKC::Math::Vect2<int>;
-
+using skc_Vect2d = SKC::Math::Vect2d;
+using skc_Vect2f = SKC::Math::Vect2f;
+using skc_Vect2i = SKC::Math::Vect2i;
 
 //TODO(SKC): move to own file.
 // this should be in own file because there is no other reason that format is included in this file
